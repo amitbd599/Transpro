6b322c1ec59426de6898b227ddfe6fd3fbdd5880
amitbd591 <122339814+amitbd591@users.noreply.github.com>
2023-03-21 10:32:21 +0600

    Service Two add

--- src/components/ServiceTwo.js
+++ src/components/ServiceTwo.js
@@ -1,6 +1,30 @@
 import React from "react";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import Slider from "react-slick";
 
 const ServiceTwo = () => {
+  function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return <FaArrowLeft className={className} onClick={onClick} />;
+  }
+  function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return <FaArrowRight className={className} onClick={onClick} />;
+  }
+  const settings = {
+    dots: false,
+    arrows: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    initialSlide: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    cssEase: "linear",
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+  };
   return (
     <>
       {/* service area start */}
@@ -29,114 +53,152 @@ const ServiceTwo = () => {
             </div>
           </div>
           <div className='service-slider owl-carousel'>
-            <div className='item'>
-              <div
-                className='single-service-wrap-2'
-                style={{ backgroundImage: "url(assets/img/service/10.png)" }}
-              >
-                <div className='thumb'>
-                  <div className='icon'>
-                    <img
-                      src='assets/img/service/service-icon-001.png'
-                      alt='img'
-                    />
-                    <img
-                      className='img-2'
-                      src='assets/img/service/service-icon-01.png'
-                      alt='img'
-                    />
+            <Slider {...settings}>
+              <div className='item'>
+                <div
+                  className='single-service-wrap-2'
+                  style={{ backgroundImage: "url(assets/img/service/10.png)" }}
+                >
+                  <div className='thumb'>
+                    <div className='icon'>
+                      <img
+                        src='assets/img/service/service-icon-001.png'
+                        alt='img'
+                      />
+                      <img
+                        className='img-2'
+                        src='assets/img/service/service-icon-01.png'
+                        alt='img'
+                      />
+                    </div>
                   </div>
-                </div>
-                <div className='details'>
-                  <h5>SEA TRANSPORTATION</h5>
-                  <p>
-                    Energistically reconceptualize ubiquitous solution wherea
-                    market-driven expertise. Synergistical empower parallel
-                    processes with highly efficient infomediaries.
-                  </p>
-                  <div className='btn-wrap'>
-                    <a className='read-more-text' href='service-details.html'>
-                      READ MORE{" "}
-                      <span>
-                        <i className='fa fa-arrow-right' />
-                      </span>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className='item'>
-              <div
-                className='single-service-wrap-2'
-                style={{ backgroundImage: "url(assets/img/service/10.png)" }}
-              >
-                <div className='thumb'>
-                  <div className='icon'>
-                    <img
-                      src='assets/img/service/service-icon-002.png'
-                      alt='img'
-                    />
-                    <img
-                      className='img-2'
-                      src='assets/img/service/service-icon-02.png'
-                      alt='img'
-                    />
-                  </div>
-                </div>
-                <div className='details'>
-                  <h5>AIR TRANSPORTATION</h5>
-                  <p>
-                    Energistically reconceptualize ubiquitous solution wherea
-                    market-driven expertise. Synergistical empower parallel
-                    processes with highly efficient infomediaries.
-                  </p>
-                  <div className='btn-wrap'>
-                    <a className='read-more-text' href='service-details.html'>
-                      READ MORE{" "}
-                      <span>
-                        <i className='fa fa-arrow-right' />
-                      </span>
-                    </a>
+                  <div className='details'>
+                    <h5>SEA TRANSPORTATION</h5>
+                    <p>
+                      Energistically reconceptualize ubiquitous solution wherea
+                      market-driven expertise. Synergistical empower parallel
+                      processes with highly efficient infomediaries.
+                    </p>
+                    <div className='btn-wrap'>
+                      <a className='read-more-text' href='service-details.html'>
+                        READ MORE{" "}
+                        <span>
+                          <FaArrowRight />
+                        </span>
+                      </a>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className='item'>
-              <div
-                className='single-service-wrap-2'
-                style={{ backgroundImage: "url(assets/img/service/10.png)" }}
-              >
-                <div className='thumb'>
-                  <div className='icon'>
-                    <img
-                      src='assets/img/service/service-icon-003.png'
-                      alt='img'
-                    />
-                    <img
-                      className='img-2'
-                      src='assets/img/service/service-icon-03.png'
-                      alt='img'
-                    />
+              <div className='item'>
+                <div
+                  className='single-service-wrap-2'
+                  style={{ backgroundImage: "url(assets/img/service/10.png)" }}
+                >
+                  <div className='thumb'>
+                    <div className='icon'>
+                      <img
+                        src='assets/img/service/service-icon-002.png'
+                        alt='img'
+                      />
+                      <img
+                        className='img-2'
+                        src='assets/img/service/service-icon-02.png'
+                        alt='img'
+                      />
+                    </div>
                   </div>
-                </div>
-                <div className='details'>
-                  <h5>WAREHOUSING</h5>
-                  <p>
-                    Energistically reconceptualize ubiquitous solution wherea
-                    market-driven expertise. Synergistical empower parallel
-                    processes with highly efficient infomediaries.
-                  </p>
-                  <div className='btn-wrap'>
-                    <a className='read-more-text' href='service-details.html'>
-                      READ MORE{" "}
-                      <span>
-                        <i className='fa fa-arrow-right' />
-                      </span>
-                    </a>
+                  <div className='details'>
+                    <h5>AIR TRANSPORTATION</h5>
+                    <p>
+                      Energistically reconceptualize ubiquitous solution wherea
+                      market-driven expertise. Synergistical empower parallel
+                      processes with highly efficient infomediaries.
+                    </p>
+                    <div className='btn-wrap'>
+                      <a className='read-more-text' href='service-details.html'>
+                        READ MORE{" "}
+                        <span>
+                          <FaArrowRight />
+                        </span>
+                      </a>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+              <div className='item'>
+                <div
+                  className='single-service-wrap-2'
+                  style={{ backgroundImage: "url(assets/img/service/10.png)" }}
+                >
+                  <div className='thumb'>
+                    <div className='icon'>
+                      <img
+                        src='assets/img/service/service-icon-003.png'
+                        alt='img'
+                      />
+                      <img
+                        className='img-2'
+                        src='assets/img/service/service-icon-03.png'
+                        alt='img'
+                      />
+                    </div>
+                  </div>
+                  <div className='details'>
+                    <h5>WAREHOUSING</h5>
+                    <p>
+                      Energistically reconceptualize ubiquitous solution wherea
+                      market-driven expertise. Synergistical empower parallel
+                      processes with highly efficient infomediaries.
+                    </p>
+                    <div className='btn-wrap'>
+                      <a className='read-more-text' href='service-details.html'>
+                        READ MORE{" "}
+                        <span>
+                          <FaArrowRight />
+                        </span>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className='item'>
+                <div
+                  className='single-service-wrap-2'
+                  style={{ backgroundImage: "url(assets/img/service/10.png)" }}
+                >
+                  <div className='thumb'>
+                    <div className='icon'>
+                      <img
+                        src='assets/img/service/service-icon-002.png'
+                        alt='img'
+                      />
+                      <img
+                        className='img-2'
+                        src='assets/img/service/service-icon-02.png'
+                        alt='img'
+                      />
+                    </div>
+                  </div>
+                  <div className='details'>
+                    <h5>AIR TRANSPORTATION</h5>
+                    <p>
+                      Energistically reconceptualize ubiquitous solution wherea
+                      market-driven expertise. Synergistical empower parallel
+                      processes with highly efficient infomediaries.
+                    </p>
+                    <div className='btn-wrap'>
+                      <a className='read-more-text' href='service-details.html'>
+                        READ MORE{" "}
+                        <span>
+                          <FaArrowRight />
+                        </span>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Slider>
           </div>
         </div>
       </div>
