4ca4315de3e461345c375f3c6089cbf40a645fae
amitbd591 <122339814+amitbd591@users.noreply.github.com>
2023-03-21 11:45:49 +0600

    Skill One add

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,11 @@
 import React from "react";
+import AboutOne from "../components/AboutOne";
 import Breadcrumb from "../components/Breadcrumb";
+import CounterOne from "../components/CounterOne";
 import NavbarOne from "../components/NavbarOne";
+import PortfolioOne from "../components/PortfolioOne";
+import SkillOne from "../components/SkillOne";
+import VideoAreaOne from "../components/VideoAreaOne";
 import SearchPopup from "../elements/SearchPopup";
 
 const About = () => {
@@ -14,6 +19,23 @@ const About = () => {
 
       {/* Breadcrumb */}
       <Breadcrumb title={"ABOUT US"} />
+
+      {/* About One */}
+      <div className='pd-top-120 pd-bottom-120'>
+        <AboutOne />
+      </div>
+
+      {/* Counter One */}
+
+      <div classname='fact-area' style={{ background: "#f9f9f9" }}>
+        <CounterOne />
+      </div>
+
+      {/* Skill One */}
+      <SkillOne />
+
+      {/* Video Area One */}
+      <VideoAreaOne />
     </>
   );
 };
