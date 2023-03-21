9dc553dafcc84058605a83009750923eb7a910f2
amitbd591 <122339814+amitbd591@users.noreply.github.com>
2023-03-21 10:53:54 +0600

    Video Area One add

--- src/components/VideoAreaOne.js
+++ src/components/VideoAreaOne.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaPhoneAlt, FaPlay } from "react-icons/fa";
 
 const VideoAreaOne = () => {
   return (
@@ -15,7 +16,7 @@ const VideoAreaOne = () => {
             href='https://www.youtube.com/embed/Wimkqo8gDZ0'
             data-effect='mfp-zoom-in'
           >
-            <i className='fa fa-play' />
+            <FaPlay />
           </a>
         </div>
         <div className='container'>
@@ -33,23 +34,7 @@ const VideoAreaOne = () => {
                 </div>
                 <div className='single-cta-wrap'>
                   <div className='icon'>
-                    <svg
-                      className='svg-inline--fa fa-phone-alt fa-w-16'
-                      aria-hidden='true'
-                      focusable='false'
-                      data-prefix='fa'
-                      data-icon='phone-alt'
-                      role='img'
-                      xmlns='http://www.w3.org/2000/svg'
-                      viewBox='0 0 512 512'
-                      data-fa-i2svg=''
-                    >
-                      <path
-                        fill='currentColor'
-                        d='M497.39 361.8l-112-48a24 24 0 0 0-28 6.9l-49.6 60.6A370.66 370.66 0 0 1 130.6 204.11l60.6-49.6a23.94 23.94 0 0 0 6.9-28l-48-112A24.16 24.16 0 0 0 122.6.61l-104 24A24 24 0 0 0 0 48c0 256.5 207.9 464 464 464a24 24 0 0 0 23.4-18.6l24-104a24.29 24.29 0 0 0-14.01-27.6z'
-                      />
-                    </svg>
-                    {/* <i class="fa fa-phone-alt"></i> Font Awesome fontawesome.com */}
+                    <FaPhoneAlt />
                   </div>
                   <div className='details'>
                     <h6>Have Any Question</h6>
