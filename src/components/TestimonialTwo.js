83829d4e9a2d650b5df9abcad50c46af22057dfa
amitbd591 <122339814+amitbd591@users.noreply.github.com>
2023-03-21 10:50:21 +0600

    Testimonial Two add

--- src/components/TestimonialTwo.js
+++ src/components/TestimonialTwo.js
@@ -1,6 +1,30 @@
 import React from "react";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import Slider from "react-slick";
 
 const TestimonialTwo = () => {
+  function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return <FaArrowLeft className={className} onClick={onClick} />;
+  }
+  function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return <FaArrowRight className={className} onClick={onClick} />;
+  }
+  const settings = {
+    dots: false,
+    arrows: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    initialSlide: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    cssEase: "linear",
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+  };
   return (
     <>
       {/* testimonial area start */}
@@ -22,69 +46,92 @@ const TestimonialTwo = () => {
             </div>
           </div>
           <div className='testimonial-slider owl-carousel'>
-            <div className='item'>
-              <div className='single-testimonial-wrap'>
-                <div className='icon'>
-                  <img src='assets/img/testimonial/quote.png' alt='img' />
-                </div>
-                <p>
-                  “Progressively strategize intermandated manufactured products
-                  after multidisci plinary sources. Conveniently iterate
-                  value-added systems with.”
-                </p>
-                <div className='client-wrap'>
-                  <div className='thumb'>
-                    <img src='assets/img/testimonial/1.png' alt='img' />
+            <Slider {...settings}>
+              <div className='item'>
+                <div className='single-testimonial-wrap'>
+                  <div className='icon'>
+                    <img src='assets/img/testimonial/quote.png' alt='img' />
                   </div>
-                  <div className='details'>
-                    <h5>ANJELINA MATHEUS</h5>
-                    <p>Co-Founder</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className='item'>
-              <div className='single-testimonial-wrap'>
-                <div className='icon'>
-                  <img src='assets/img/testimonial/quote.png' alt='img' />
-                </div>
-                <p>
-                  “Progressively strategize intermandated manufactured products
-                  after multidisci plinary sources. Conveniently iterate
-                  value-added systems with.”
-                </p>
-                <div className='client-wrap'>
-                  <div className='thumb'>
-                    <img src='assets/img/testimonial/2.png' alt='img' />
-                  </div>
-                  <div className='details'>
-                    <h5>MONISH POUL</h5>
-                    <p>Co-Founder</p>
+                  <p>
+                    “Progressively strategize intermandated manufactured
+                    products after multidisci plinary sources. Conveniently
+                    iterate value-added systems with.”
+                  </p>
+                  <div className='client-wrap'>
+                    <div className='thumb'>
+                      <img src='assets/img/testimonial/1.png' alt='img' />
+                    </div>
+                    <div className='details'>
+                      <h5>ANJELINA MATHEUS</h5>
+                      <p>Co-Founder</p>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className='item'>
-              <div className='single-testimonial-wrap'>
-                <div className='icon'>
-                  <img src='assets/img/testimonial/quote.png' alt='img' />
-                </div>
-                <p>
-                  “Progressively strategize intermandated manufactured products
-                  after multidisci plinary sources. Conveniently iterate
-                  value-added systems with.”
-                </p>
-                <div className='client-wrap'>
-                  <div className='thumb'>
-                    <img src='assets/img/testimonial/3.png' alt='img' />
+              <div className='item'>
+                <div className='single-testimonial-wrap'>
+                  <div className='icon'>
+                    <img src='assets/img/testimonial/quote.png' alt='img' />
                   </div>
-                  <div className='details'>
-                    <h5>MICHEL CLACRK</h5>
-                    <p>Co-Founder</p>
+                  <p>
+                    “Progressively strategize intermandated manufactured
+                    products after multidisci plinary sources. Conveniently
+                    iterate value-added systems with.”
+                  </p>
+                  <div className='client-wrap'>
+                    <div className='thumb'>
+                      <img src='assets/img/testimonial/2.png' alt='img' />
+                    </div>
+                    <div className='details'>
+                      <h5>MONISH POUL</h5>
+                      <p>Co-Founder</p>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+              <div className='item'>
+                <div className='single-testimonial-wrap'>
+                  <div className='icon'>
+                    <img src='assets/img/testimonial/quote.png' alt='img' />
+                  </div>
+                  <p>
+                    “Progressively strategize intermandated manufactured
+                    products after multidisci plinary sources. Conveniently
+                    iterate value-added systems with.”
+                  </p>
+                  <div className='client-wrap'>
+                    <div className='thumb'>
+                      <img src='assets/img/testimonial/3.png' alt='img' />
+                    </div>
+                    <div className='details'>
+                      <h5>MICHEL CLACRK</h5>
+                      <p>Co-Founder</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className='item'>
+                <div className='single-testimonial-wrap'>
+                  <div className='icon'>
+                    <img src='assets/img/testimonial/quote.png' alt='img' />
+                  </div>
+                  <p>
+                    “Progressively strategize intermandated manufactured
+                    products after multidisci plinary sources. Conveniently
+                    iterate value-added systems with.”
+                  </p>
+                  <div className='client-wrap'>
+                    <div className='thumb'>
+                      <img src='assets/img/testimonial/2.png' alt='img' />
+                    </div>
+                    <div className='details'>
+                      <h5>MONISH POUL</h5>
+                      <p>Co-Founder</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Slider>
           </div>
         </div>
       </div>
